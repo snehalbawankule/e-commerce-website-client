4f6d8bb42a12f07deb2129d74794aa0a8996e69a
snehalbawankule <snehalbawankule2000@gmail.com>
2023-06-05 16:44:11 +0530

    commit

--- src/components/products/product-list.tsx
+++ src/components/products/product-list.tsx
@@ -1,65 +1,116 @@
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
+
 import { useAppDispatch, useAppSelector } from "../../hooks/hooks";
 import { Grid } from "@mui/material";
 import ProductCard from "./product-card";
 import { useLocation } from "react-router-dom";
 import { addProduct } from "../../store/product/services";
-import { useEffect, useState } from "react";
 
 const ProductList = () => {
   const location = useLocation();
   const currentPage = location.pathname.slice(1);
-
+  console.log("currentpage" + currentPage);
   const dispatch = useAppDispatch();
+  const [data, setData] = useState<any[]>([]);
 
-  const Products = useAppSelector((state) => state.products.product);
-  const [products, setProducts] = useState(Products);
+  const [pageNo, setPageNo] = useState(1);
+  const [isLoading, setIsLoading] = useState(true);
+  const previousInputValue = useRef("");
+
+  console.log("aswa" + currentPage !== previousInputValue.current);
   useEffect(() => {
-    if (products.length) {
-      dispatch(addProduct());
+    previousInputValue.current = currentPage;
 
-      fetch(
-        `http://localhost:3001/get-category-products?page=1&size=12&sort=createdAt&order=ASC&category=${currentPage}`
-      )
-        .then((res) => res.json())
-        .then((json) => {
-          setProducts(json);
-        });
+    console.log("pre" + previousInputValue.current);
+    if (currentPage !== previousInputValue.current) {
+      console.log("hi");
+      window.location.reload();
+      getData(currentPage);
+    } else {
+      getData(currentPage);
     }
-  }, [products.length, dispatch, currentPage]);
+  }, [currentPage, isLoading]);
+  console.log(previousInputValue.current);
+  const history = currentPage;
+  console.log(history);
+
+  function getData(currentPage: string) {
+    fetch(
+      `http://localhost:3001/get-category-products?page=${pageNo}&size=5&sort=createdAt&order=ASC&category=${currentPage}`
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        if (pageNo > 1) {
+          let arr = [...data, ...json];
+
+          setData(arr);
+          setIsLoading(false);
+        } else {
+          setData(json);
+          setIsLoading(false);
+        }
+      })
+      // .then((response) => {
+      //   if (pageNo > 1) {
+      //     let arr = [...data, ...response.data];
+
+      //     setData(arr);
+      //   } else {
+      //     setData(response.data);
+      //   }
+      // })
+      .catch((error) => {
+        alert("Axios GET request failed");
+      });
+  }
+
+  const firstEvent = (e: any) => {
+    var bottom =
+      e.target.scrollHeight - e.target.scrollTop - e.target.clientHeight < 50;
+    if (bottom) {
+      let pg = pageNo + 1;
+      setPageNo(pg);
+      getData(currentPage);
+    }
+  };
 
   return (
-    <>
-      <Grid container>
-        <Grid
-          container
-          direction="row"
-          spacing={2}
-          marginLeft="250px"
-          style={{
-            marginBottom: 50,
-            marginTop: 80,
-            paddingBottom: 20,
-          }}
-        >
-          {products.map((post: any, index: any) => {
-            return (
-              <Grid
-                item
-                xs={12}
-                sm={6}
-                md={3}
-                lg={3}
-                marginTop="15px"
-                display="flex"
-                key={index}
-              >
-                <ProductCard post={post} />
-              </Grid>
-            );
-          })}
-        </Grid>
+    <Grid
+      container
+      onScroll={firstEvent}
+      style={{ height: 600, overflowY: "auto" }}
+    >
+      <Grid
+        container
+        direction="row"
+        spacing={2}
+        marginLeft="250px"
+        style={{
+          marginBottom: 50,
+          marginTop: 80,
+          paddingBottom: 20,
+        }}
+      >
+        {data.map((post: any, index: any) => {
+          return (
+            <Grid
+              item
+              xs={12}
+              sm={6}
+              md={3}
+              lg={3}
+              marginTop="15px"
+              display="flex"
+              key={index}
+            >
+              <ProductCard post={post} />
+            </Grid>
+          );
+        })}
       </Grid>
-    </>
+    </Grid>
   );
 };
+
 export default ProductList;
